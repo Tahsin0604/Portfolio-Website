72bc6d03439476d05179dc067c557631f40b12c5
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-07-03 18:12:40 +0600

    icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
 import "./globals.css";
 import { Roboto } from "next/font/google";
-
+import { Metadata } from "next";
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
   variable: "--font-roboto",
 });
-
+// export const metadata: Metadata = {
+//   icons:{
+//     icon:
+//   }
+// };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
